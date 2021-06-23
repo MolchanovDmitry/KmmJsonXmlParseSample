26771928d04072e3590aeba0d63073d05f1027df
dmolchanov <dmolchanov@uma.tech>
2021-06-23 18:01:58 +0300

    вернул пример ios

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -2,21 +2,10 @@ import SwiftUI
 import shared
 
 struct ContentView: View {
-    let vm = MainViewModel()
-    var jsonText: Text
-    
-    init(){
-        vm.loadTitleFromJson()
-    }
+    let greeting = Greeting().greeting()
 
 	var body: some View {
-        Text(vm.greeting)
-        let jsonText = Text("")
-        vm.jsonTitle.bind { [weak self](title) in
-            guard let self = self else {return}
-            jsonText.setText(title)
-        }
-        jsonText
+        Text(greeting)
 	}
 }
 
