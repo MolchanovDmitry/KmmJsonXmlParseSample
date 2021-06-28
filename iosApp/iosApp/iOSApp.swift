3be74bd8647db296f476d98e2957bde97dc40117
dmolchanov <dmolchanov@uma.tech>
2021-06-28 21:00:06 +0300

    помаранчева революцiя

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -2,9 +2,12 @@ import SwiftUI
 
 @main
 struct iOSApp: App {
+    
+    @UIApplicationDelegateAdaptor(DimaDelegate.self) var appDelegate
+    
 	var body: some Scene {
 		WindowGroup {
-			ContentView()
+			//ContentView()
 		}
 	}
 }
