97392489be6769ad4112e002e519cae8c958d864
dmolchanov <dmolchanov@uma.tech>
2021-06-30 07:54:53 +0300

    Рефакторинг android view части

--- androidApp/src/main/java/com/dmitry/molchanov/kmmjsonxmlparsesample/android/MainActivity.kt
+++ androidApp/src/main/java/com/dmitry/molchanov/kmmjsonxmlparsesample/android/MainActivity.kt
@@ -5,10 +5,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.dmitry.molchanov.kmmjsonxmlparsesample.domain.XmlInteractor
 import com.dmitry.molchanov.kmmjsonxmlparsesample.viewmodel.AndroidMainViewModel
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,28 +15,33 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        vm.loadTitleFromJson()
-
+        // Печатаем приветствие
         setTextViewText(R.id.text_view, vm.greeting)
 
-        GlobalScope.launch {
-            val xml = XmlInteractor()
-            val totalPrice = xml.getTotalPriceFromXml()
+        // Запрашиваем url c XML
+        vm.getTotalPriceFromXml()
+        // Подписываемся на изменение значения общей суммы из распаршенного XML
+        vm.totalPrice.bind { totalPrice ->
             val text = "Total price: $totalPrice"
             setTextViewText(R.id.xml_view, text)
         }
 
+        // Запрашиваем url с JSON
+        vm.loadTitleFromJson()
+        // Подписываемся на изменение значения заголовка из распаршенного JSON
         vm.jsonTitle.bind { result ->
             val text = result?.let { "Json: $it" } ?: "error"
             setTextViewText(R.id.json_view, text)
         }
 
         val editText = findViewById<EditText>(R.id.edit_text)
+        // Подписываемся на изменение значения из БД
         vm.dbValue.bind(editText::setText)
+        // Берем из поля значение и записываем в БД
         findViewById<Button>(R.id.set_db_value).setOnClickListener {
             vm.setDbValue(editText.text.toString())
         }
-
+        // Запрос на чтение из БД
         findViewById<Button>(R.id.get_db_value_button).setOnClickListener {
             vm.getDbValue()
         }
--- shared/src/androidMain/kotlin/com/dmitry/molchanov/kmmjsonxmlparsesample/viewmodel/AndroidMainViewModel.kt
+++ shared/src/androidMain/kotlin/com/dmitry/molchanov/kmmjsonxmlparsesample/viewmodel/AndroidMainViewModel.kt
@@ -2,6 +2,19 @@ package com.dmitry.molchanov.kmmjsonxmlparsesample.viewmodel
 
 import android.content.Context
 import com.dmitry.molchanov.kmmjsonxmlparsesample.db.DriverFactory
+import com.dmitry.molchanov.kmmjsonxmlparsesample.domain.XmlInteractor
+import kotlinx.coroutines.launch
 
 class AndroidMainViewModel(context: Context) :
-    MainViewModel(sqlDriver = DriverFactory(context).createDriver())
+    MainViewModel(sqlDriver = DriverFactory(context).createDriver()) {
+
+    val totalPrice: LiveData<Float> = LiveData()
+
+    private val xmlInteractor = XmlInteractor()
+
+    fun getTotalPriceFromXml() {
+        scope.launch {
+            totalPrice.value = xmlInteractor.getTotalPriceFromXml()
+        }
+    }
+}
